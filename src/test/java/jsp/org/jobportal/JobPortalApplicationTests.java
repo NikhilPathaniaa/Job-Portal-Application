d2dab08e4de7ee5efb8883e9a6865b3229134558
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-03-28 16:26:52 +0530

    first commit

---
+++ src/test/java/jsp/org/jobportal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package jsp.org.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
