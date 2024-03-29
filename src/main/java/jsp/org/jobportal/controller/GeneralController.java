775c3f1a323357c44bfb00cb2ba8b7c0055989aa
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-03-29 17:32:18 +0530

    first commit

--- src/main/java/jsp/org/jobportal/controller/GeneralController.java
+++ src/main/java/jsp/org/jobportal/controller/GeneralController.java
@@ -1,8 +1,14 @@
 package jsp.org.jobportal.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
+import jsp.org.jobportal.dto.PortalUser;
 
 @Controller
 public class GeneralController {
@@ -26,8 +32,12 @@ public class GeneralController {
 	}
 	
 	@PostMapping("/signup")
-	public String signup()
+	public String signup(@Valid PortalUser user,BindingResult result)
 	{
+		if(LocalDate.now().getYear()-user.getDob().getYear()<18)
+			result.rejectValue("dob", "error.dob","* Age should be greater Than 18");
+		if(!user.getPassword().equals(user.getConfirm_password()))
+			result.rejectValue("confirm_password", "error.confirm_password","* Password and Confirm_password should be matching");
 		return "login.html";
 	}
 }
--- src/main/java/jsp/org/jobportal/dto/PortalUser.java
+++ src/main/java/jsp/org/jobportal/dto/PortalUser.java
@@ -2,13 +2,30 @@ package jsp.org.jobportal.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class PortalUser {
 
+	@Size(min = 3,max = 15,message="* Enter between 3~15 charactes")
 	private String name;
+	@NotNull(message = "* this is required field")
+	@Email(message = "* Enter Proper Email format")
 	private String email;
+	@DecimalMin(value="6000000000", message = "* Enter Proper Mobile Number")
+	@DecimalMax(value="9999999999", message = "* Enter Proper Mobile Number")
 	private long mobile;
+	@NotNull(message = "* this is required field")
 	private LocalDate dob;
+	@Size(min = 3,max = 18,message="* Enter between 3~18 charactes")
 	private String password;
+	@Size(min = 3,max = 18,message="* Enter between 3~18 charactes")
 	private String confirm_password; 
+	@NotNull(message = "* this is required field")
 	private String role;
 }
